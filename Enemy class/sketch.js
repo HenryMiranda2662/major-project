39583bf68353a9648a975f15acc96247ff172d77
HenryMiranda2662 <47368811+HenryMiranda2662@users.noreply.github.com>
2019-05-01 14:57:45 -0600

    added level 1,2 and 3 enemies

--- Enemy class/sketch.js
+++ Enemy class/sketch.js
@@ -1,17 +1,18 @@
-// Project Title
-// Your Name
+// MAJOR PROJECT
+// Henry Miranda
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
 class EnemyBall {
-  constructor(x, y, someSpeed) {
+  constructor(x, y, dxSomeSpeed, dySomeSpeed) {
     this.x = x;
     this.y = y;
     this.color = "red";
     this.radius = 25;
-    this.speed = someSpeed;
+    this.dx = dxSomeSpeed;
+    this.dy = dySomeSpeed;
   }
   display() {
     fill(this.color);
@@ -19,35 +20,161 @@ class EnemyBall {
     ellipse(this.x, this.y, this.radius * 2);
   }
   move() {
-    this.y += this.speed;
+
+    this.x += this.dx;
+    this.y += this.dy;
+
+    if (this.x + this.radius >= width || this.x - this.radius <= 0) {
+      this.dx = -1 * this.dx;
+    }
+
     if (this.y + this.radius >= height || this.y - this.radius <= 0) {
-      this.speed = -1 * this.speed;
+      this.dy = -1 * this.dy;
     }
 
   }
 }
 
-let EnemyBall1;
-let EnemyBall2;
-let EnemyBall3;
+let enemyball1Level1;
+let enemyball2Level1;
+let enemyball3Level1;
+let enemyball1Level2;
+let enemyball2Level2;
+let enemyball3Level2;
+let enemyball1Level3;
+let enemyball2Level3;
+let enemyball3Level3;
 
 function setup() {
   createCanvas(500, 500);
+  state = "menu"
+  level1Button = {
+    x: 50,
+    y: 100,
+    width: 113,
+    height: 75,
+  }
+  level2Button = {
+    x: 200,
+    y: 100,
+    width: 113,
+    height: 75,
+  }
+  level3Button = {
+    x: 350,
+    y: 100,
+    width: 113,
+    height: 75,
+  }
+  
+  
 
-  enemyBall1 = new EnemyBall(100, height / 2, 17);
-  enemyBall2 = new EnemyBall(width / 2, height / 2, 9);
-  enemyBall3 = new EnemyBall(400, height / 2, 5);
+  enemyball1Level1 = new EnemyBall(100, height/2, 0, 17);
+  enemyball2Level1 = new EnemyBall(width / 2, height/2, 0, 9);
+  enemyball3Level1 = new EnemyBall(400, height/2, 0, 5);
+
+  enemyball1Level2 = new EnemyBall(100, height/2, random(5, 6), random(5, 6));
+  enemyball2Level2 = new EnemyBall(width / 2, height/2, random(5, 6), random(5, 6));
+  enemyball3Level2 = new EnemyBall(400, height/2, random(5, 6), random(5, 6));
+  
+  enemyball1Level3 = new EnemyBall(width/2, 100, 12, 0);
+  enemyball2Level3 = new EnemyBall(width/2, height/2, 12, 0);
+  enemyball3Level3 = new EnemyBall(width/2, 400, 12, 0);
 }
 
 function draw() {
-  enemyBall1.move();
-  enemyBall2.move();
-  enemyBall3.move();
+  if (state === "menu") {
+    background(80, 170, 200);
+    rect(level1Button.x, level1Button.y, level1Button.width, level1Button.height);
+    rect(level2Button.x, level2Button.y, level2Button.width, level2Button.height);
+    rect(level3Button.x, level3Button.y, level3Button.width, level3Button.height);
+  }
+  if (state === "Level1") {
+    enemyBallLevel1();
+  }
+  if (state === "Level2") {
+    enemyBallLevel2();
+  }
+  if (state === "Level3") {
+    enemyBallLevel3();
+  }
+}
 
-  background(200);
+  function enemyBallLevel1() {
+    enemyball1Level1.move();
+    enemyball2Level1.move();
+    enemyball3Level1.move();
 
-  enemyBall1.display();
-  enemyBall2.display();
-  enemyBall3.display();
+    background("white")
+
+    enemyball1Level1.display();
+    enemyball2Level1.display();
+    enemyball3Level1.display();
+  }
+
+  function enemyBallLevel2() {
+    enemyball1Level2.move();
+    enemyball2Level2.move();
+    enemyball3Level2.move();
+
+    background("white");
+
+    enemyball1Level2.display();
+    enemyball2Level2.display();
+    enemyball3Level2.display();
+}
+
+function enemyBallLevel3(){
+    enemyball1Level3.move();
+    enemyball2Level3.move();
+    enemyball3Level3.move();
+
+    background("white");
+
+    enemyball1Level3.display();
+    enemyball2Level3.display();
+    enemyball3Level3.display();
+
+}
+
+function clickedOnButtonL1(x, y) {
+  return x >= level1Button.x &&
+    x <= level1Button.x + level1Button.width &&
+    y >= level1Button.y &&
+    y <= level1Button.y + level1Button.height;
+}
+
+function clickedOnButtonL2(x, y) {
+  return x >= level2Button.x &&
+    x <= level2Button.x + level2Button.width &&
+    y >= level2Button.y &&
+    y <= level2Button.y + level2Button.height;
+}
+
+function clickedOnButtonL3(x, y) {
+  return x >= level3Button.x &&
+    x <= level3Button.x + level3Button.width &&
+    y >= level3Button.y &&
+    y <= level3Button.y + level3Button.height;
+}
+
+function mousePressed() {
+  if (state === "menu") {
+    if (clickedOnButtonL1(mouseX, mouseY)) {
+      state = "Level1";
+    }
+  }
+
+  if (state === "menu") {
+    if (clickedOnButtonL2(mouseX, mouseY)) {
+      state = "Level2";
+    }
+  }
+  
+  if (state === "menu") {
+    if (clickedOnButtonL3(mouseX, mouseY)) {
+      state = "Level3";
+    }
+  }
 }
 
