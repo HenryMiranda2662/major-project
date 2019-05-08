db6c25345de85ccfcdc5ec49bd9347297742f0f0
HenryMiranda2662 <47368811+HenryMiranda2662@users.noreply.github.com>
2019-05-08 14:46:54 -0600

    combined both codes into one
    colition for levels 1-5

--- sketch.js
+++ sketch.js
@@ -78,6 +78,15 @@ let enemyball3Level2;
 let enemyball1Level3;
 let enemyball2Level3;
 let enemyball3Level3;
+let enemyball1Level4;
+let enemyball2Level4;
+let enemyball3Level4;
+let enemyball1Level5;
+let enemyball2Level5;
+let enemyball3Level5;
+let enemyball4Level5;
+let enemyball5Level5;
+let enemyball6Level5;
 
 function setup() {
   createCanvas(500, 500);
@@ -100,6 +109,18 @@ function setup() {
     width: 113,
     height: 75,
   }
+  level4Button = {
+    x: 50,
+    y: 200,
+    width: 113,
+    height: 75,
+  }
+  level5Button = {
+    x: 200,
+    y: 200,
+    width: 113,
+    height: 75,
+  }
 
   player1 = new PlayerBall(31, height / 1.5, 4, 0);
   player2 = new PlayerBall(31, height/1.5, 4, 4);
@@ -115,16 +136,30 @@ function setup() {
   enemyball1Level3 = new EnemyBall(width/2, 100, 12, 0);
   enemyball2Level3 = new EnemyBall(width/2, height/2, 12, 0);
   enemyball3Level3 = new EnemyBall(width/2, 400, 12, 0);
+
+  enemyball1Level4 = new EnemyBall(width / 2, height/2, 5, -5);
+  enemyball2Level4 = new EnemyBall(width / 2, height/2, 0, 9);
+  enemyball3Level4 = new EnemyBall(width / 2, height/2, 5, 5);
+
+  enemyball1Level5 = new EnemyBall(100, height/2, 0, 7);
+  enemyball2Level5 = new EnemyBall(width / 2, height/2, 0, 12);
+  enemyball3Level5 = new EnemyBall(400, height/2, 0, 7);
+  enemyball4Level5 = new EnemyBall(width/2, 100, 12, 0);
+  enemyball5Level5 = new EnemyBall(width/2, height/2, 7, 0);
+  enemyball6Level5 = new EnemyBall(width/2, 400, 12, 0);
 }
 
 function draw() {
   if (state === "menu") {
     background(80, 170, 200);
     fill("white");
+    noStroke();
     rect(level1Button.x, level1Button.y, level1Button.width, level1Button.height);
     rect(level2Button.x, level2Button.y, level2Button.width, level2Button.height);
     rect(level3Button.x, level3Button.y, level3Button.width, level3Button.height);
-
+    rect(level4Button.x, level4Button.y, level4Button.width, level4Button.height);
+    rect(level5Button.x, level5Button.y, level5Button.width, level5Button.height);
+    
     restartPositions();
 
   }
@@ -148,6 +183,20 @@ function draw() {
     background("white");
     level3BallDisplay();
     itHitLevel3();
+  }
+
+  if (state === "Level4") {
+    Level4BallMovement();
+    background("white");
+    Level4BallDisplay();
+    itHitLevel4();
+  }
+
+  if (state === "Level5") {
+    Level5BallMovement();
+    background("white");
+    Level5BallDisplay();
+    itHitLevel5();
   }
 }
 
@@ -212,6 +261,41 @@ function level3BallDisplay(){
   player2.display();
 }
 
+function Level4BallMovement() {
+  enemyball1Level4.move();
+  enemyball2Level4.move();
+  enemyball3Level4.move();
+  player2.move();
+}
+
+function Level4BallDisplay(){
+  enemyball1Level4.display();
+  enemyball2Level4.display();
+  enemyball3Level4.display();
+  player2.display();
+
+}
+
+function Level5BallMovement() {
+  enemyball1Level5.move();
+  enemyball2Level5.move();
+  enemyball3Level5.move();
+  enemyball4Level5.move();
+  enemyball5Level5.move();
+  enemyball6Level5.move();
+  player2.move();
+}
+
+function Level5BallDisplay(){
+  enemyball1Level5.display();
+  enemyball2Level5.display();
+  enemyball3Level5.display();
+  enemyball4Level5.display();
+  enemyball5Level5.display();
+  enemyball6Level5.display();
+  player2.display();
+}
+
 function itHitLevel1() {
   // Just like the other level, it checks the distance between the player, and the enemies, if they're too close
   // you lose and go back to the "menu"
@@ -260,6 +344,44 @@ function itHitLevel3() {
   }
 }
 
+function itHitLevel4() {
+  // Just like the other level, it checks the distance between the player, and the enemies, if they're too close
+  // you lose and go back to the "menu"
+  distanceAwayFromCenter1 = int(dist(player2.x, player2.y, enemyball1Level4.x, enemyball1Level4.y));                   
+  distanceAwayFromCenter2 = int(dist(player2.x, player2.y, enemyball2Level4.x, enemyball2Level4.y));
+  distanceAwayFromCenter3 = int(dist(player2.x, player2.y, enemyball3Level4.x, enemyball3Level4.y));
+  
+  collitionDistance1 = (player1.radius + enemyball1Level4.radius);
+  
+  if (distanceAwayFromCenter1  <= collitionDistance1 ||
+      distanceAwayFromCenter2 <= collitionDistance1 || 
+      distanceAwayFromCenter3  <= collitionDistance1)  {
+    state = "menu";
+  }
+}
+
+function itHitLevel5() {
+  // Just like the other level, it checks the distance between the player, and the enemies, if they're too close
+  // you lose and go back to the "menu"
+  distanceAwayFromCenter1 = int(dist(player2.x, player2.y, enemyball1Level5.x, enemyball1Level5.y));                   
+  distanceAwayFromCenter2 = int(dist(player2.x, player2.y, enemyball2Level5.x, enemyball2Level5.y));
+  distanceAwayFromCenter3 = int(dist(player2.x, player2.y, enemyball3Level5.x, enemyball3Level5.y));
+  distanceAwayFromCenter4 = int(dist(player2.x, player2.y, enemyball4Level5.x, enemyball4Level5.y));                   
+  distanceAwayFromCenter5 = int(dist(player2.x, player2.y, enemyball5Level5.x, enemyball5Level5.y));
+  distanceAwayFromCenter6 = int(dist(player2.x, player2.y, enemyball6Level5.x, enemyball6Level5.y));
+  
+  collitionDistance1 = (player2.radius + enemyball1Level4.radius);
+  
+  if (distanceAwayFromCenter1  <= collitionDistance1 ||
+      distanceAwayFromCenter2 <= collitionDistance1 || 
+      distanceAwayFromCenter3  <= collitionDistance1 ||
+      distanceAwayFromCenter4 <= collitionDistance1 ||
+      distanceAwayFromCenter5 <= collitionDistance1 ||
+      distanceAwayFromCenter6 <= collitionDistance1) {
+    state = "menu";
+  }
+}
+
 function clickedOnButtonL1(x, y) {
   return x >= level1Button.x &&
     x <= level1Button.x + level1Button.width &&
@@ -281,6 +403,20 @@ function clickedOnButtonL3(x, y) {
     y <= level3Button.y + level3Button.height;
 }
 
+function clickedOnButtonL4(x, y) {
+  return x >= level4Button.x &&
+    x <= level4Button.x + level4Button.width &&
+    y >= level4Button.y &&
+    y <= level4Button.y + level4Button.height;
+}
+
+function clickedOnButtonL5(x, y) {
+  return x >= level5Button.x &&
+    x <= level5Button.x + level5Button.width &&
+    y >= level5Button.y &&
+    y <= level5Button.y + level5Button.height;
+}
+
 function mousePressed() {
   if (state === "menu") {
     if (clickedOnButtonL1(mouseX, mouseY)) {
@@ -297,6 +433,18 @@ function mousePressed() {
   if (state === "menu") {
     if (clickedOnButtonL3(mouseX, mouseY)) {
       state = "Level3";
+    }
+  }
+
+  if (state === "menu") {
+    if (clickedOnButtonL4(mouseX, mouseY)) {
+      state = "Level4";
+    }
+  }
+
+  if (state === "menu") {
+    if (clickedOnButtonL5(mouseX, mouseY)) {
+      state = "Level5";
     }
   }
 }
